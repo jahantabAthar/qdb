0b3fb25d055d65f44caf4b411b7c379c5fa0e9d4
Muhammad Jahantab <muhammad.jahantab@pitb.gov.pk>
2024-07-24 14:42:45 +0500

    'refine'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,14 @@
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
-
+import Header from "@/components/Header";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [loading, setLoading] = useState<boolean>(true);
-
-  // const pathname = usePathname();
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
@@ -19,9 +17,27 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 ">
-          {loading ? <Loader /> : children}
+      <body>
+        <div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="flex">
+                <div className="relative flex flex-1 flex-col ">
+                  <Header />
+                  <main>
+                    <div className="mx-auto max-w-screen-1xl p-4 md:p-6 1xl:p-10 relative -top-30 min-h-100">
+                      <div className="mx-auto max-w-screen-3xl pl-50 pr-50">
+                        <Breadcrumb pageName={"Company Setup"} />
+                      </div>
+                      {children}
+                    </div>
+                  </main>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </body>
     </html>
